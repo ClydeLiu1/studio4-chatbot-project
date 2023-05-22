efa32445a253c780e6002008858d4272ba6743c7
pubba98 <coolpubba@gmail.com>
2023-05-23 00:40:29 +1200

    sample desing

--- studio4-chatbot-project/petfoodstore/chatbot.php
+++ studio4-chatbot-project/petfoodstore/chatbot.php
@@ -5,37 +5,44 @@
   <style>
     body {
       font-family: Arial, Helvetica, sans-serif;
-    }
-
-    * {
-      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
     }
 
     /* Chat icon */
     .chat-icon {
       background-color: #4285F4;
       color: white;
+      padding: 16px;
       border: none;
       cursor: pointer;
       opacity: 0.8;
       position: fixed;
-      bottom: 20px;
-      right: 20px;
+      bottom: 23px;
+      right: 28px;
       width: 60px;
       height: 60px;
       border-radius: 50%;
       text-align: center;
       line-height: 60px;
       font-size: 24px;
-      transition: background-color 0.3s, right 0.3s, bottom 0.3s;
+      animation: chatIconAnimation 2s infinite;
     }
 
-    /* Chat icon on hover */
-    .chat-icon:hover {
-      background-color: #0D47A1;
+    /* Animation for chat icon */
+    @keyframes chatIconAnimation {
+      0% {
+        transform: translateX(0);
+      }
+      50% {
+        transform: translateX(-20px);
+      }
+      100% {
+        transform: translateX(0);
+      }
     }
 
-    /* The popup chat - hidden by default */
+    /* Chat popup */
     .chat-popup {
       display: none;
       position: fixed;
@@ -43,135 +50,142 @@
       right: 20px;
       width: 320px;
       background-color: #fff;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      z-index: 99;
       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+      border-radius: 5px;
+      z-index: 9999;
     }
 
-    /* Chat header */
+    .open {
+      display: block !important;
+    }
+
+    .close {
+      display: none;
+    }
+
     .chat-header {
-      padding: 15px;
       background-color: #4285F4;
-      color: white;
-      border-top-left-radius: 4px;
-      border-top-right-radius: 4px;
+      color: #fff;
+      padding: 12px;
+      font-weight: bold;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
     }
 
-    /* Chat content */
-    .chat-content {
-      padding: 15px;
+    .chat-body {
+      padding: 20px;
       max-height: 300px;
       overflow-y: auto;
     }
 
-    /* Chat input */
+    .chat-message {
+      margin-bottom: 15px;
+    }
+
+    .chat-sender {
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    .chat-timestamp {
+      font-size: 12px;
+      color: #888;
+    }
+
     .chat-input {
       display: flex;
       align-items: center;
-      padding: 10px;
-      background-color: #f2f2f2;
+      margin-top: 10px;
     }
 
     .chat-input textarea {
       flex: 1;
-      padding: 8px;
-      border: none;
       resize: none;
-      background-color: #fff;
-      border-radius: 4px;
+      border-radius: 3px;
+      border: 1px solid #ddd;
+      padding: 10px;
       font-size: 14px;
     }
 
     .chat-input button {
-      margin-left: 10px;
       background-color: #4285F4;
-      color: white;
+      color: #fff;
       border: none;
-      padding: 8px 15px;
-      border-radius: 4px;
-      cursor: pointer;
+      padding: 10px 15px;
       font-size: 14px;
-      transition: background-color 0.3s;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
     }
 
     .chat-input button:hover {
-      background-color: #0D47A1;
+      background-color: #1967D2;
     }
 
-    /* Chat message style */
-    .chat-message {
-      margin-bottom: 10px;
-    }
-
-    .chat-message .sender {
-      font-weight: bold;
-      color: #4285F4;
-    }
-
-    .chat-message .timestamp {
-      font-size: 12px;
-      color: gray;
+    .close-form {
+      position: absolute;
+      top: 5px;
+      right: 10px;
+      color: #fff;
+      cursor: pointer;
     }
   </style>
 </head>
 <body>
 
-  <h2>Popup Chat Window</h2>
-  <p>Click on the chat icon at the bottom of this page to open the chat form.</p>
-  <p>Note that the chat icon and the form are fixed - they will always be positioned at the bottom of the browser window.</p>
+  <div class="chat-icon" id="chatIcon" onclick="toggleChatPopup()">
+    <span>&#9993;</span>
+  </div>
 
-  <button class="chat-icon" onclick="toggleForm()">&#9993;</button>
-
-  <div class="chat-popup" id="myForm">
+  <div class="chat-popup" id="chatPopup">
     <div class="chat-header">
-      <h1>Chat</h1>
-      <button class="close-button" onclick="closeForm()">&times;</button>
+      Chat
+      <span class="close-form" onclick="toggleChatPopup()">&#10006;</span>
     </div>
-    <div class="chat-content" id="chatMessages"></div>
+    <div class="chat-body" id="chatBody">
+      <!-- Chat messages will be displayed here -->
+    </div>
     <div class="chat-input">
-      <textarea id="messageInput" placeholder="Type message.." required></textarea>
-      <button type="button" onclick="sendMessage()">Send</button>
+      <textarea id="messageInput" placeholder="Type a message..."></textarea>
+      <button onclick="sendMessage()">Send</button>
     </div>
   </div>
 
   <script>
-    const chatMessages = document.getElementById("chatMessages");
-    const messageInput = document.getElementById("messageInput");
-    const chatIcon = document.querySelector(".chat-icon");
-    const chatPopup = document.getElementById("myForm");
+    var chatPopup = document.getElementById("chatPopup");
+    var chatIcon = document.getElementById("chatIcon");
+    var chatBody = document.getElementById("chatBody");
+    var messageInput = document.getElementById("messageInput");
 
-    function toggleForm() {
-      if (chatPopup.style.display === "block") {
-        closeForm();
-      } else {
-        openForm();
-      }
-    }
-
-    function openForm() {
-      chatPopup.style.display = "block";
-      chatIcon.style.right = "340px";
-      chatIcon.style.bottom = "120px";
-    }
-
-    function closeForm() {
-      chatPopup.style.display = "none";
-      chatIcon.style.right = "20px";
-      chatIcon.style.bottom = "20px";
+    function toggleChatPopup() {
+      chatPopup.classList.toggle("open");
+      chatIcon.classList.toggle("close");
     }
 
     function sendMessage() {
-      const message = messageInput.value;
-      if (message.trim() !== "") {
-        const sender = "You";
-        const timestamp = new Date().toLocaleTimeString();
+      var message = messageInput.value.trim();
+      if (message !== "") {
+        var sender = "You";
+        var timestamp = new Date().toLocaleTimeString();
 
-        const chatMessage = document.createElement("div");
+        var chatMessage = document.createElement("div");
         chatMessage.classList.add("chat-message");
-        chatMessage.innerHTML = `<span class="sender">${sender}:</span> ${message} <span class="timestamp">${timestamp}</span>`;
+        chatMessage.innerHTML = `
+          <div class="chat-sender">${sender}</div>
+          <div>${message}</div>
+          <div class="chat-timestamp">${timestamp}</div>
+        `;
 
-        chatMessages.appendChild(chatMessage);
+        var responseMessage = document.createElement("div");
+        responseMessage.classList.add("chat-message", "response");
+        responseMessage.innerHTML = `
+          <div class="chat-sender">Chatbot</div>
+          <div>This is a response to your message: "${message}"</div>
+          <div class="chat-timestamp">${timestamp}</div>
+        `;
+
+        chatBody.appendChild(chatMessage);
+        chatBody.appendChild(responseMessage);
+        chatBody.scrollTop = chatBody.scrollHeight;
         messageInput.value = "";
       }
     }
