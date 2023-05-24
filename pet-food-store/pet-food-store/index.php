12f7447b48af216c9f032aa68e9a3e31f83f972d
ClydeLiu1 <93743139+ClydeLiu1@users.noreply.github.com>
2023-05-24 23:22:31 +1200

    update

--- pet-food-store/pet-food-store/index.php
+++ pet-food-store/pet-food-store/index.php
@@ -1,7 +1,3 @@
-<?php 
-include 'connection.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +8,7 @@ include 'connection.php';
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto+Condensed:wght@300;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/chatbot.css">
 
 </head>
 <body>
@@ -27,16 +24,10 @@ include 'connection.php';
             <h1>Pet Ruff</h1>
             <h2>Your pets' favourite place</h2>
             
-</div>
-	   
-	  
-       
-      
-	  
-        
-     </header>
+        </div>
+    </header>
 
-     <section>
+    <section>
         <div class="container">
             <h3>Best place for all your pets needs</h3>
             <p>
@@ -58,7 +49,7 @@ include 'connection.php';
                 <h3>Dog Food</h3>
                 <a href="dogFood.php"><img src="images/dog_food.jpg" class="imgFood"></a>
                 <p>20% on selected items</p>
-            </div>
+              </div>
             </div>
             
             <div class="column">
@@ -76,131 +67,73 @@ include 'connection.php';
                 <p>Discounts over $20 purchases</p>
               </div>
             </div>
-          </div>
-     </section>
+        </div>
+    </section>
 
-     <footer>
+    <footer>
         <br>
         <p>Author: Pudubu Dasun<br>
         <a href="mailto:info@petruff.com">info@petruff.com</a></p>
-     </footer>
-    
-    </body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Pet Food Store Chatbot</title>
-  <style>
-    /* Chatbot container styles */
-    .chatbot-container {
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      width: 300px;
-      height: 400px;
-      background-color: #fff;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      overflow: hidden;
-    }
+    </footer>
 
-    /* Chatbot header styles */
-    .chatbot-header {
-      background-color: #000;
-      color: #fff;
-      padding: 10px;
-      text-align: center;
-    }
-
-    /* Chatbot body styles */
-    .chatbot-body {
-      padding: 10px;
-      height: 330px;
-      overflow-y: scroll;
-    }
-
-    /* Chat message styles */
-    .message {
-      margin-bottom: 10px;
-    }
-
-    /* User message styles */
-    .user-message {
-      text-align: right;
-    }
-
-    /* Chatbot message styles */
-    .chatbot-message {
-      text-align: left;
-    }
-
-    /* Chat input styles */
-    .chat-input {
-      width: 100%;
-      padding: 5px;
-    }
-  </style>
-</head>
-<body>
-  <div class="chatbot-container">
-    <div class="chatbot-header">
-      <h3>Chatbot</h3>
+    <div class="chatbot-container">
+        <div class="chatbot-header">
+            <h3>Chatbot</h3>
+        </div>
+        <div class="chatbot-body">
+            <!-- Chat messages will be displayed here -->
+        </div>
+        <div class="chat-input-container">
+            <input type="text" id="chat-input" class="chat-input" placeholder="Type your message..." />
+            <button id="send-button" class="send-button">Send</button>
+        </div>
     </div>
-    <div class="chatbot-body">
-      <!-- Chat messages will be displayed here -->
-    </div>
-    <div class="chat-input-container">
-      <input type="text" class="chat-input" placeholder="Type your message..." />
-    </div>
-  </div>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      // Retrieve chat messages from the API and display them
-      function fetchChatMessages() {
-        // Make an API request to retrieve chat messages
-        $.ajax({
-          url: "api_endpoint_url",
-          method: "GET",
-          success: function(response) {
-            // Process the response and display chat messages
-            var messages = response.messages;
-            var chatContainer = $(".chatbot-body");
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Function to add a new message to the chat
+            function addMessage(sender, content) {
+                var chatContainer = $(".chatbot-body");
+                var senderClass = sender === "user" ? "user-message" : "chatbot-message";
 
-            chatContainer.empty();
+                chatContainer.append('<div class="message ' + senderClass + '">' + content + '</div>');
 
-            for (var i = 0; i < messages.length; i++) {
-              var message = messages[i];
-
-              if (message.sender === "user") {
-                chatContainer.append('<div class="message user-message">' + message.content + '</div>');
-              } else {
-                chatContainer.append('<div class="message chatbot-message">' + message.content + '</div>');
-              }
+                // Scroll to the bottom of the chat container
+                chatContainer.scrollTop(chatContainer.prop("scrollHeight"));
             }
 
-            // Scroll to the bottom of the chat container
-            chatContainer.scrollTop(chatContainer.prop("scrollHeight"));
-          },
-          error: function() {
-            console.error("Failed to fetch chat messages.");
-          }
+            // Handle send button click or Enter key press
+            function handleSendMessage() {
+                var messageInput = $("#chat-input");
+                var message = messageInput.val().trim();
+
+                if (message !== "") {
+                    addMessage("user", message);
+                    messageInput.val("");
+                    getChatbotResponse(message);
+                }
+            }
+
+            // Get chatbot response from the API
+            function getChatbotResponse(message) {
+                // Make a POST request to the PHP file
+                $.post("chatbot.php", {message: message}, function(response) {
+                    addMessage("chatbot", response);
+                });
+            }
+
+            // Bind send button click event
+            $("#send-button").click(handleSendMessage);
+
+            // Bind Enter key press event
+            $("#chat-input").keypress(function(event) {
+                if (event.which === 13) {
+                    handleSendMessage();
+                }
+            });
         });
-      }
+    </script>
 
-      // Send user messages to the API and update the chat
-      function sendMessage(message) {
-        // Make an API request to send the user message
-        $.ajax({
-          url: "api_endpoint_url",
-          method: "POST",
-          data: { message: message },
-          success: function(response) {
-            // Fetch the updated chat messages
-            fetchChatMessages();
-         
-
-
-
+</body>
+</html>
